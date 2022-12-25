0b763ee01907daa98b7a34f0cd1803b1dbcaa6e2
rashini <rashininanayakkara@gmail.com>
2022-12-25 17:33:47 +0530

    Add controller/EmployeeController.java

--- src/main/java/com/swanws/EmpMgt/controller/EmployeeController.java
+++ src/main/java/com/swanws/EmpMgt/controller/EmployeeController.java
@@ -1,13 +1,17 @@
-package com.swanws.EmpMgt;
+package com.swanws.EmpMgt.controller;
 
 import java.net.URI;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,11 +28,13 @@ public class EmployeeController {
 
 	@GetMapping
 	public Employees getEmployees() {
+		System.out.println("get All");
 		 return employeeDao.getAllEmployees();
 	}
 
 	@PostMapping(path = "/", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) {
+		System.out.println("Add");
 		Integer id = employeeDao.getAllEmployees().getEmployeeList().size() + 1;
 		employee.setId(id);
 
@@ -39,5 +45,29 @@ public class EmployeeController {
 
 		return ResponseEntity.created(location).build();
 	}
-
+	
+	@GetMapping("/{id}")
+	public Employee getEmployeeById(@PathVariable("id") int id) {
+		System.out.println("by id");
+		return employeeDao.getEmployeeById(id);
+	}
+	
+	@RequestMapping(params = "fname")
+	public Employee getEmployeeByFirstName(@RequestParam("fname") String fname) {
+		System.out.println("by first name");
+		return employeeDao.getEmployeeByFirstName(fname);
+	}
+	
+	@RequestMapping(params = "lname")
+	public Employee getEmployeeByLastName(@RequestParam("lname") String lname) {
+		System.out.println("by last name");
+		return employeeDao.getEmployeeByLastName(lname);
+	}
+	
+	@RequestMapping( params = {"fname", "lname" })
+	public Employee getEmployeeByFnameAndLname(@RequestParam(value="fname")  String fname, @RequestParam(value= "lname") String lname) {
+		System.out.println("by fname and lname" + fname + lname);
+		return employeeDao.getEmployeeByFirstNameAndLastName(fname, lname);
+	}
+	
 }
--- src/main/java/com/swanws/EmpMgt/dao/EmployeeDAO.java
+++ src/main/java/com/swanws/EmpMgt/dao/EmployeeDAO.java
@@ -8,20 +8,59 @@ import com.swanws.EmpMgt.model.Employees;
 @Repository
 public class EmployeeDAO {
 	private static Employees list = new Employees();
-	
-	static
-    {
-        list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
-        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
-        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
-    }
-	
-	 public Employees getAllEmployees() 
-	    {
-	        return list;
-	    }
-	     
-	    public void addEmployee(Employee employee) {
-	        list.getEmployeeList().add(employee);
-	    }
+
+	static {
+		list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
+		list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+		list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+	}
+
+	public Employees getAllEmployees() {
+		return list;
+	}
+
+	public void addEmployee(Employee employee) {
+		list.getEmployeeList().add(employee);
+	}
+
+	public Employee getEmployeeById(int id) {
+		for (Employee emp : list.getEmployeeList()) {
+			if (emp.getId() == id) {
+				return emp;
+			}
+		}
+
+		return null;
+	}
+
+	public Employee getEmployeeByFirstName(String name) {
+		for (Employee emp : list.getEmployeeList()) {
+			if (emp.getFirstName().equals(name)) {
+				return emp;
+			}
+		}
+		return null;
+	}
+
+	public Employee getEmployeeByLastName(String name) {
+		for (Employee emp : list.getEmployeeList()) {
+			if (emp.getLastName().equals(name)) {
+				return emp;
+			}
+		}
+		return null;
+	}
+
+	public Employee getEmployeeByFirstNameAndLastName(String fname, String lname) {
+		if (!fname.equals(null) && !lname.equals(null)) {	
+			for (Employee emp : list.getEmployeeList()) {
+				if (emp.getFirstName().equals(fname) && emp.getLastName().equals(lname)) {
+					System.out.println(emp);
+					return emp;
+				}
+			}
+		} 
+		return null;
+	}
+
 }
